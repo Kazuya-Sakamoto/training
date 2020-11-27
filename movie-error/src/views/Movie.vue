8570f9dee7f15e5df898cd9034ac201ac4f39095
坂本一弥 <sskazuya1130@gmail.com>
2020-11-27 16:59:47 +0900

    HEVC 動画表示

---
+++ movie-error/src/views/Movie.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+
+const initialState = () => ({
+  is_mute: true,
+  video: false
+});
+
+export default defineComponent({
+  name: 'Movie',
+  components: {},
+
+  setup() {
+    const state = reactive(initialState())
+
+    return {
+      ...toRefs(state),
+    }
+  }
+});
+</script>
+
+<template>
+  <section>
+    <div class="movie-card">
+      <!-- AAC、H.264 -->
+      <video src="../assets/movies/a.mp4" controls />
+    </div>
+    <div class="movie-card">
+      <!-- AAC、HEVC -->
+      <video src="../assets/movies/H.264.mp4" controls />
+    </div>
+    <div class="movie-card">
+      <!-- AAC、HEVC -->
+      <video src="../assets/movies/b.mp4" controls />
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+section {
+  .movie-card {
+    video {
+      width: 400px;
+      height: 150px;
+    }
+  }
+}
+</style>
