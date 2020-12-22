8c6fb972db979aa4d51d9456ea482904a691a9ca
坂本一弥 <sskazuya1130@gmail.com>
2020-12-22 16:56:43 +0900

    イラストロジック

--- logic/script.js
+++ logic/script.js
@@ -1,16 +1,14 @@
 // * 要素 HTML表示
 for (let i = 0; i < 10; i++) {
-  let tr = $('<tr>');
+  let tr = $('<tr>'); //10行
   for (var j = 0; j < 10; j++) {
-    var mathNum = i * 10 + j + 1;
+    var mathNum = i * 10 + j + 1; //箱 番号
     tr.append($('<td>').text(mathNum));
   }
   $('#table').append(tr);  
 }
 
 let recorderBtn = true;
-let recordFlag = false;
-
 // * 録画クリックする
 const recorderClick = () => {
   if(recorderBtn) {
@@ -24,28 +22,33 @@ const recorderClick = () => {
     isSession();
   }
 };
-
-// * ボタンテキスト変える
+// * 録画ボタン テキスト変換
 const btnChange = (value) => {
   document.getElementById("text").innerHTML = value;
 }
-
 // * 要素押下後、色を変える
 const trigger = document.querySelectorAll('td');
 const array = []
 for(var i = 0; i < trigger.length ; i++) {
   trigger[i].onclick = (e) => {
-    e.path[0].classList.toggle('color')
-    console.log(e)
-    array.push(e.target.innerHTML)
+    e.path[0].classList.toggle('color') // path: Array(8) [0] == td
     if(document.getElementById("text").innerHTML == "録画中") {
+      array.push(e.target.innerHTML)
       console.log("sessionに保存中")
       sessionStorage.setItem("クリックした値", [array])
     } 
   }
 }
+// * セッションストレージに値が存在するか確認する
+const isSession = () => {
+  if(sessionStorage.getItem('クリックした値') == null) {
+    document.getElementById("alert").innerHTML = "";
+  } else {
+    document.getElementById("alert").innerHTML = "録画記録あり";
+  }
+}
 
-// * 再生 ボタン(保存)
+// * 再生 ボタン (保存)
 const regenerateClick = () => {
   if(sessionStorage.getItem('クリックした値') == null) {
     isSession();
@@ -57,12 +60,10 @@ const regenerateClick = () => {
     isSession();
   }
 };
-
 // *再現 console
 const interval = (times, arraySession) => {
-	let timer_id;
   let i = 0;
-	timer_id = setInterval((function(){
+	const timer_id = setInterval((function(){
     demonstrate(arraySession[i])
     i++;
     if (i == times) {
@@ -74,23 +75,10 @@ const interval = (times, arraySession) => {
 // * 再現 template
 const demonstrate = (value) => {
   var triggerEnd = document.querySelectorAll('.color');
-  console.log(triggerEnd)
   console.log(value)
-
   for(var i = 0; i < triggerEnd.length ; i++) {
-    console.log(triggerEnd[i].innerText)
     if(triggerEnd[i].innerText == value ) {
-      console.log("一致するよ！")
       triggerEnd[i].classList.toggle('demo')
     }
-  }
-}
-
-// * セッションストレージに値が存在するか確認する
-const isSession = () => {
-  if(sessionStorage.getItem('クリックした値') == null) {
-    document.getElementById("alert").innerHTML = "";
-  } else {
-    document.getElementById("alert").innerHTML = "録画記録あり";
   }
 }
